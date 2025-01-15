c86abb2b300326019835156bac8c1c0d7b8039ad
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2025-01-15 15:12:26 -0300

    Adicionada anotações

--- Polimorfismo/src/br/ufop/sobrescrita/MainClass.java
+++ Polimorfismo/src/br/ufop/sobrescrita/MainClass.java
@@ -5,11 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		Pagamento pagamento1 = new PagamentoCartao();
-        Pagamento pagamento2 = new PagamentoPIX();
+		Pagamento pagamento2 = new PagamentoPIX();
 
-        pagamento1.processarPagamento(250.00); 
-        pagamento2.processarPagamento(500.00); 
-		
+		pagamento1.processarPagamento(250.00);
+		pagamento2.processarPagamento(500.00);
+
+//        pagamento1.exibirHistoricoPIX();  ERRO - conceito de Upcasting: não é acessível pela instância da subclasse ser do tipo da superclasse, 
+// 		  assim seus métodos não são acessíveis nesse objeeto criado
+
 	}
 
 }
--- Polimorfismo/src/br/ufop/sobrescrita/PagamentoPIX.java
+++ Polimorfismo/src/br/ufop/sobrescrita/PagamentoPIX.java
@@ -6,5 +6,9 @@ public class PagamentoPIX extends Pagamento {
 	public void processarPagamento(double valor) {  // Sobrescrita do método processarPagamento na subclasse
 		System.out.println("Processando pagamento via PIX no valor de R$ " + valor);
 	}
+	
+	public void exibirHistoricoPIX() {
+		System.out.println("Exibindo histórico PIX...");
+	}
 
 }
